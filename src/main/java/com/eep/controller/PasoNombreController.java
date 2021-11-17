4b57cb3f809f55746d62cc423d540ea1c850e8fa
Mario <maguirre@eepmad.com>
2021-11-17 15:44:43 +0100

    17/11/2021 - EEP

--- src/main/java/com/eep/controller/PasoNombreController.java
+++ src/main/java/com/eep/controller/PasoNombreController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.eep.model.Escuela;
-
 @Controller
 @RequestMapping("/ejercicios")
 public class PasoNombreController {
@@ -16,18 +14,14 @@ public class PasoNombreController {
 
 	@GetMapping("/ejercicio1")
 	public String helloWorld(Model model) {
-		Escuela eep = new Escuela();
-		eep.setNombre("EEP iGroup");
-		model.addAttribute("escuela", eep);
+		model.addAttribute("nombre", "EEP iGroup");
 		return plantillaActividad2;
 	}
 
 	@RequestMapping(value = "/ejercicio1mav", method = RequestMethod.GET)
 	public ModelAndView helloWorldMAV() {
 		ModelAndView mav = new ModelAndView(plantillaActividad2);
-		Escuela eep = new Escuela();
-		eep.setNombre("EEP iGroup");
-		mav.addObject("escuela", eep);
+		mav.addObject("nombre", "EEP iGroup");
 		return mav;
 	}
 }
