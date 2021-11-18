5de602e2464e1b85415eaf6fb10cc320c38ba410
Mario <maguirre@eepmad.com>
2021-11-18 15:43:36 +0100

    18/11/2021 - EEP

---
+++ src/main/java/com/eep/model/Alumno.java
@@ -0,0 +1,32 @@
+package com.eep.model;
+
+public class Alumno {
+	private String nombre;
+	private String apellidos;
+
+	public Alumno() {
+
+	}
+
+	public Alumno(String nombre, String apellidos) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+}
